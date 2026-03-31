ecde80dd15e24c330c8247c310008b780e5c4358
Joakim-Pham <joakim070504@gmail.com>
2026-03-31 19:35:19 +0200

    make log in accsess for the website change url view.py made login html for website.

--- listings/views.py
+++ listings/views.py
@@ -1,15 +1,35 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Listing, Amenity, Booking, Review
+from django.contrib.auth.decorators import login_required
 
+from .models import Listing, Amenity, Booking, Review
 
 def home(request):
     return render(request, "listings/home.html")
 
-
+@login_required
 def listing_list(request):
     listings = Listing.objects.all()
     return render(request, "listings/listing_list.html", {"listings": listings})
 
+@login_required
+def listing_detail(request, listing_id):
+    listing = get_object_or_404(Listing, id=listing_id)
+    return render(request, "listings/listing_detail.html", {"listing": listing})
+
+@login_required
+def amenity_list(request):
+    amenities = Amenity.objects.all()
+    return render(request, "listings/amenity_list.html", {"amenities": amenities})
+
+@login_required
+def booking_list(request):
+    bookings = Booking.objects.all()
+    return render(request, "listings/booking_list.html", {"bookings": bookings})
+
+@login_required
+def review_list(request):
+    reviews = Review.objects.all()
+    return render(request, "listings/review_list.html", {"reviews": reviews})
 
 def listing_detail(request, listing_id):
     listing = get_object_or_404(Listing, id=listing_id)
